723422e8beabc0466f8d9922467d0c92375b7cca
xiaoling <virtueDe@163.com>
2024-04-26 09:36:50 +0800

    chore: pro init

---
+++ packages/app/src/pages/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts" generic="T extends any, O extends any">
+defineOptions({
+  name: 'IndexPage',
+})
+
+// const name = ref('')
+
+// const router = useRouter()
+// function go() {
+//   if (name.value)
+//     router.push(`/hi/${encodeURIComponent(name.value)}`)
+// }
+</script>
+
+<template>
+  <div>
+    123123
+    <!-- <TheInput v-model="name" placeholder="What's your name?" autocomplete="false" @keydown.enter="go" /> -->
+
+    <!-- <div>
+      <button class="m-3 text-sm btn" :disabled="!name" @click="go">
+        Go
+      </button>
+    </div> -->
+  </div>
+</template>
